b4a7597c197a88cb5fcaa7857eba78eb36efe240
Monzurul Hasan <monzurulhasan1001@gmail.com>
2025-11-23 18:04:09 +0600

    feat: add php support

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -27,11 +27,31 @@ fn run_njs(code: String) -> String {
     result
 }
 
+#[command]
+fn run_php(code: String) -> String {
+    let mut child = Command::new("php")
+        .arg("-r")
+        .stdin(Stdio::null())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .arg(code)
+        .spawn()
+        .expect("failed to run php");
+
+    let output = child.wait_with_output().unwrap();
+
+    let mut result = String::new();
+    result.push_str(&String::from_utf8_lossy(&output.stdout));
+    result.push_str(&String::from_utf8_lossy(&output.stderr));
+
+    result
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![run_njs])
+        .invoke_handler(tauri::generate_handler![run_php, run_njs])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useState, useEffect, useRef } from 'react';
 import CodeMirror from '@uiw/react-codemirror';
 import { javascript } from '@codemirror/lang-javascript';
+import { php } from '@codemirror/lang-php';
 import { aura } from '@uiw/codemirror-theme-aura';
 import { invoke } from '@tauri-apps/api/core';
 
@@ -8,25 +9,34 @@ export default function App() {
   const [code, setCode] = useState("");
   const [output, setOutput] = useState('');
   const debounceRef = useRef<number | null>(null);
+  const [language, setLanguage] = useState<string>("js");
 
   useEffect(() => {
-    const savedCode = localStorage.getItem('js_live_code');
+    const savedCode = localStorage.getItem('code_value');
     if (savedCode) setCode(savedCode);
     else setCode("console.log('Hello World!');");
   }, []);
 
   const onChange = useCallback((value: string, _: any) => {
     setCode(value);
-    localStorage.setItem('js_live_code', value);
+    localStorage.setItem('code_value', value);
 
     if (debounceRef.current) clearTimeout(debounceRef.current);
     debounceRef.current = setTimeout(() => {
-      runCode(value);
+      runCode(value, language);
     }, 500);
   }, []);
 
-  async function runCode(currentCode: string = code) {
-    const output: string = await invoke("run_njs", { code: currentCode });
+  async function runCode(currentCode: string = code, language: string) {
+    const languages: Record<string, string> = {
+      php: "run_php",
+      js: "run_njs"
+    };
+    let finalCode = currentCode;
+    if (language === 'php' && currentCode.toLowerCase().startsWith('<?php')) {
+      finalCode = code.substring(5);
+    }
+    const output: string = await invoke(languages[language], { code: finalCode });
     setOutput(output);
   }
 
@@ -35,12 +45,12 @@ export default function App() {
       <div className="flex-1 flex flex-col border-b md:border-b-0 md:border-r border-gray-700">
         <div className="p-4 w-full font-bold text-lg border-b border-gray-700 flex justify-between items-center">
           <div>JS Live</div>
-          <button
-            onClick={() => runCode()}
-            className="bg-green-600 hover:bg-green-700 px-4 py-1 text-sm cursor-pointer rounded-sm font-semibold"
-          >
-            Run
-          </button>
+          <div>
+            <select onChange={(e) => setLanguage(e.target.value)} defaultValue={'js'} className='w-38 px-1 bg-gray-800 text-white'>
+              <option value="js">JavaScript</option>
+              <option value="php">PHP</option>
+            </select>
+          </div>
         </div>
         <div className="flex-1">
           <CodeMirror
@@ -48,15 +58,20 @@ export default function App() {
             height="100%"
             className='h-full'
             theme={aura}
-            extensions={[javascript({ jsx: true })]}
+            extensions={[javascript(), php()]}
             onChange={onChange}
           />
         </div>
       </div>
 
       <div className="flex-1 flex flex-col">
-        <div className="p-4 font-bold text-lg border-b border-gray-700">
-          Output
+        <div className="p-4 text-right font-bold text-lg border-b border-gray-700">
+          <button
+            onClick={() => runCode(code, language)}
+            className="bg-green-600 hover:bg-green-700 px-4 py-1 text-sm cursor-pointer rounded-sm font-semibold"
+          >
+            Run
+          </button>
         </div>
         <pre className="flex-1 select-all p-2 px-4 overflow-auto bg-gray-800 text-sm font-[monospace]">{output}</pre>
       </div>
